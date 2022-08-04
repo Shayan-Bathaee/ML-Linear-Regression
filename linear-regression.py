640f8c4bc30a98d069a43d6aa984b76f92a1ffba
Shayan-Bathaee <shayanbathaee@gmail.com>
2022-08-03 17:03:11 -0700

    There is a problem with it. The line appears above the points by the amount of the minimum y value it seems

--- linear-regression.py
+++ linear-regression.py
@@ -5,16 +5,73 @@
 # Concepts were inspired by the following article: https://towardsdatascience.com/linear-regression-using-gradient-descent-97a6c8700931
 
 
+from ctypes import wstring_at
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import sys
+import openpyxl
+
+# DEFINE FUNCTION TO GET X AND Y DATA FROM SPREADSHEET
+def getDataFromSpreadsheet(filename):
+    wb = openpyxl.load_workbook(filename)
+    ws = wb.active
+    global xname
+    global yname
+    xname = ws["A1"].value
+    yname = ws["B1"].value
+    xvalues = []
+    yvalues = []
+    for cell in ws["A"]:
+        xvalues.append(cell.value)
+    xvalues.pop(0)                  # pop the name
+    for cell in ws["B"]:
+        yvalues.append(cell.value)
+    yvalues.pop(0)
+
+    return {xname: xvalues, yname: yvalues} # dataDictionary input to the regression initializer function
+    
+
+# DEFINE CLASS TO PERFORM REGRESSION
+class Regression:
+    # initialize the regression with the specified iterations, learning rate, and data
+    def __init__(self, limit, iterations, learningRate, dataDictionary):
+        self.limit = limit
+        self.iterations = iterations
+        self.learningRate = learningRate
+        self.m = self.b = 0
+        self.X = np.array([3,4,5,6])
+        self.Y = np.array([3,4,5,6])
+        # self.X = np.array(dataDictionary[xname])       
+        # self.Y = np.array(dataDictionary[yname])       
+        self.numDatapoints = len(self.X)                # the number of X Y pairs we hae
+        self.predictedLine = self.m*self.X + self.b     # this stores the data for the predicted line. It just starts as 0
+
+    def calculateLoss(self):
+        self.loss = 0                                               # initialize the loss to 0
+        for x in range(self.numDatapoints):                         # for each data point
+            self.loss += (self.Y[x] - (self.m*x + self.b)) ** 2     # calculate the squared error
+        self.loss = self.loss / self.numDatapoints                  # divide the summation by number of datapoints to get average loss
+        return self.loss
+
+    def learn(self):
+        self.dm = 0                                                 # initialize derivatives to 0
+        self.db = 0
+        for x in range(self.numDatapoints):                         # for each data point
+            self.dm += x*(self.Y[x] - (self.m*x + self.b))          # summation for derivative with respect to m
+            self.db += self.Y[x] - (self.m*x + self.b)              # summation for derivative with respect to b
+        self.dm = (-2/self.numDatapoints)*self.dm                   # scaling part of derivative with respect to m
+        self.db = (-2/self.numDatapoints)*self.db                   # scaling part of derivative with respect to b
+        self.m = self.m - self.learningRate*self.dm                 # adjust m and b using the derivative values and learning rate
+        self.b = self.b - self.learningRate*self.db
+        
 
 # DEFINE PARAMETERS
 if "-l" in sys.argv:
     iterations = int(sys.argv[sys.argv.index('-l') + 1])
     limit = True
 else:
+    iterations = 0
     limit = False # set to true if you would like to limit the iterations 
 
 if "-lr" in sys.argv:
@@ -23,55 +80,21 @@ else:
     learningRate = 0.005
 
 if "-i" in sys.argv:
-    print("need to do something with input")
+    filename = str(sys.argv[sys.argv.index('-i') + 1])
+    dataDictionary = getDataFromSpreadsheet(filename)
 else:
-    print("Use Default input")
-Range = 20 # define how big the graph is
+    filename = "US_GDP_example.xlsx"
+    dataDictionary = getDataFromSpreadsheet(filename)
 
-
-# DEFINE CLASS TO PERFORM REGRESSION
-class Regression:
-    def __init__(self, limit, iterations, learningRate, Range): # initialize the regression specifying iterations and range
-        self.limit = limit
-        self.iterations = iterations
-        self.learningRate = learningRate
-        self.m = self.b = 0
-        self.Range = Range
-        self.X = np.arange(0, Range + 1, 1) # X is a numpy array of integers 0 through Range
-        self.Y = self.m*self.X + self.b # this stores the data for the predicted line. It just starts as 0
-        self.data = np.random.randint(0, Range + 1, Range + 1) # random data within the range. may improve on this later
-        #self.data = self.X # this is used for testing
-
-    def calculateLoss(self):
-        self.loss = 0 # initialize the loss to 0
-        n = self.Range + 1 # n = number of data points
-        for x in range(0, n, 1): # for each data point
-            self.loss += (self.data[x] - (self.m*x + self.b)) ** 2 # calculate the squared error
-        self.loss = self.loss / n # divide the summation by n
-        return self.loss # return the loss
-
-    def learn(self):
-        self.dm = 0 # initialize derivatives to 0
-        self.db = 0
-        n = self.Range + 1 # n = number of data points
-        for x in range(0, n, 1): # for each data point
-            self.dm += x*(self.data[x] - (self.m*x + self.b)) # summation for derivative with respect to m
-            self.db += self.data[x] - (self.m*x + self.b) # summation for derivative with respect to b
-        self.dm = (-2/n)*self.dm # scaling part of derivative with respect to m
-        self.db = (-2/n)*self.db # scaling part of derivative with respect to b
-        self.m = self.m - self.learningRate*self.dm # adjust m and b using the derivative values and learning rate
-        self.b = self.b - self.learningRate*self.db
-        
 
 # INITIALIZE THE REGRESSION, INITIALIZE THE PLOT
-LR = Regression(limit, iterations, learningRate, Range)
+LR = Regression(limit, iterations, learningRate, dataDictionary)
 fig, ax = plt.subplots() # create a figure, make ax the only subplot
 ax.grid()
-# plt.figtext(0.5, 1, 'weertwer', ha="center", va="center")
-dataText = ax.text(0.02, 0.86, 'weertwer', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black'))
-X = LR.X # X is a numpy array of integers 0 through Range
-Y = LR.data # Y is a numpy array of random numbers within [0, Range]
-line, = ax.plot(X, 0*X) # start by plotting the line at 0
+dataText = ax.text(0.02, 0.86, 'm = 0\nb = 0\niterations = 0', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black'))
+X = LR.X
+Y = LR.Y
+line, = ax.plot(X, Y) # starting line is y = minimum y value
 count = 0
 consoleOutput = ""
 
@@ -83,7 +106,7 @@ def init(): # Initialize the animation
     return line, dataText # return the line 
 
 def animate(i): # This function determines what changes in each frame
-    global count
+    global count, consoleOutput
     displayString = "m = " + str(round(LR.m, 2)) + "\nb = " + str(round(LR.b, 2)) + "\niterations = " + str(i)
     if i == iterations and LR.limit == True:
         consoleOutput = displayString
@@ -98,8 +121,12 @@ def animate(i): # This function determines what changes in each frame
 # BEGIN ANIMATION AND PLOTTING
 ani = animation.FuncAnimation(fig, animate, init_func = init, interval = 1, blit = True)
 plt.plot(X, Y, 'bo') # plot the data points
-plt.ylim([0, 1.3*LR.Range]) # set the limits of the graph depending on our range (extra space added above for labels)
-plt.xlim([0, LR.Range])
+max_y = np.max(LR.Y)
+min_y = np.min(LR.Y)
+max_x = np.max(LR.X)
+min_x = np.min(LR.X)
+plt.ylim([0, max_y + 0.3*(max_y - min_y)]) # set the limits of the graph depending on our range (extra space added above for labels)
+plt.xlim([0, max_x])
 plt.show() # display the plot an animation
 # display the final slope, y intercept, and iterations
 if consoleOutput == "":
